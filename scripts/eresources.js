cd03c70b7ee68b070e423a17e4b1df77830c47fc
AJ Adversalo <ajadvers@gmail.com>
2019-04-11 12:51:35 -0700

    added logo images, updated eresources page

--- scripts/eresources.js
+++ scripts/eresources.js
@@ -1,7 +1,7 @@
 function render(eresources) {
     for (var i=0; i<resources.length; i++) {
-            document.write('<div class="card"><div class="card-header"><h3 class="name"><img src="' + eresources[i].image + '" width="100"><a href="');
-            document.write(eresources[i].link + '">' + eresources[i].title + '</a></h3></div><div class="card-body">')
+            document.write('<div class="card"><div class="card-header"><h3 class="name"><img src="' + eresources[i].image + '" width="50"><a href="');
+            document.write(eresources[i].link + '"> ' + eresources[i].title + '</a></h3></div><div class="card-body">')
             document.write('<p class="card-text">' + eresources[i].description + '</p></div></div>')
     }
 }
@@ -16,25 +16,32 @@ var resources = [
             image: 'images/visual-studio.png'},
             
             { title: 'Visual Studio Code', link: 'https://code.visualstudio.com/download', 
-            description: 'Free and open source. Integrated Git, debugging and extensions.' },
+            description: 'Free and open source. Integrated Git, debugging and extensions.',
+            image: 'images/vscode.png'},
             
             { title: 'Eclipse OXYGEN', link: 'https://www.eclipse.org/downloads/', 
-            description: 'Eclipse is an integrated development environment (IDE) used in computer programming, and is the most widely used Java IDE.' },
+            description: 'Eclipse is an integrated development environment (IDE) used in computer programming, and is the most widely used Java IDE.',
+            image: 'images/eclipse.png'},
             
             { title: 'Brackets', link: 'http://brackets.io/', 
-            description: "With focused visual tools and preprocessor support, Brackets is a modern text editor that makes it easy to design in the browser. It's crafted from the ground up for web designers and front-end developers." },
+            description: "With focused visual tools and preprocessor support, Brackets is a modern text editor that makes it easy to design in the browser. It's crafted from the ground up for web designers and front-end developers.",
+            image: 'images/brackets.jpg' },
             
             { title: 'Android Studio', link: 'https://developer.android.com/studio/index.html', 
-            description: 'Android Studio provides the fastest tools for building apps on every type of Android device.' },
+            description: 'Android Studio provides the fastest tools for building apps on every type of Android device.',
+            image: 'images/android_studio.png' },
             
             { title: 'Xcode 9', link: 'https://developer.apple.com/xcode/', 
-            description: 'With everything you need to create amazing apps for Apple platforms, Xcode 9 is unbelievably quick and consistently smooth while editing even the largest files.' },
+            description: 'With everything you need to create amazing apps for Apple platforms, Xcode 9 is unbelievably quick and consistently smooth while editing even the largest files.',
+            image: 'images/xcode.png' },
             
             { title: 'MongoDB', link: 'https://www.mongodb.com/download-center#community', 
-            description: 'MongoDB is a document database with the scalability and flexibility that you want with the querying and indexing that you need' },
+            description: 'MongoDB is a document database with the scalability and flexibility that you want with the querying and indexing that you need',
+            image: 'images/mongodb.png' },
             
             { title: 'Node.js', link: 'https://nodejs.org/en/download/', 
-            description: 'As an asynchronous event driven JavaScript runtime, Node is designed to build scalable network applications.' },
+            description: 'As an asynchronous event driven JavaScript runtime, Node is designed to build scalable network applications.',
+            image: 'images/nodejs.png' },
 ]
 
 render(resources)
